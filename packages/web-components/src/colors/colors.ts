fa6d0f8d8ab0ff4bfc39d02c139a35981b0088c5
Fatemeh <uni.khosravi@gmail.com>
2025-02-24 17:30:00 +0330

    feat: add all colors

---
+++ packages/web-components/src/colors/colors.ts
@@ -0,0 +1,338 @@
+import { rgba } from "./utils/rgb";
+
+export const black = "#09090B";
+export const black100 = black;
+
+export const transparent = "transparent";
+
+export const white = "#FFFFFF";
+export const white0 = white;
+
+export const whiteAlpha50 = rgba(white, 0.04);
+export const whiteAlpha100 = rgba(white, 0.06);
+export const whiteAlpha200 = rgba(white, 0.08);
+export const whiteAlpha300 = rgba(white, 0.16);
+export const whiteAlpha400 = rgba(white, 0.24);
+export const whiteAlpha500 = rgba(white, 0.36);
+export const whiteAlpha600 = rgba(white, 0.48);
+export const whiteAlpha700 = rgba(white, 0.64);
+export const whiteAlpha800 = rgba(white, 0.8);
+export const whiteAlpha900 = rgba(white, 0.92);
+export const whiteAlpha950 = rgba(white, 0.95);
+
+export const whiteAlpha = {
+  50: whiteAlpha50,
+  100: whiteAlpha100,
+  200: whiteAlpha200,
+  300: whiteAlpha300,
+  400: whiteAlpha400,
+  500: whiteAlpha500,
+  600: whiteAlpha600,
+  700: whiteAlpha700,
+  800: whiteAlpha800,
+  900: whiteAlpha900,
+  950: whiteAlpha950,
+};
+export const blackAlpha50 = rgba(black, 0.04);
+export const blackAlpha100 = rgba(black, 0.06);
+export const blackAlpha200 = rgba(black, 0.08);
+export const blackAlpha300 = rgba(black, 0.16);
+export const blackAlpha400 = rgba(black, 0.24);
+export const blackAlpha500 = rgba(black, 0.36);
+export const blackAlpha600 = rgba(black, 0.48);
+export const blackAlpha700 = rgba(black, 0.64);
+export const blackAlpha800 = rgba(black, 0.8);
+export const blackAlpha900 = rgba(black, 0.92);
+export const blackAlpha950 = rgba(black, 0.95);
+
+export const blackAlpha = {
+  50: blackAlpha50,
+  100: blackAlpha100,
+  200: blackAlpha200,
+  300: blackAlpha300,
+  400: blackAlpha400,
+  500: blackAlpha500,
+  600: blackAlpha600,
+  700: blackAlpha700,
+  800: blackAlpha800,
+  900: blackAlpha900,
+  950: blackAlpha950,
+};
+
+export const gray50 = "#fafafa";
+export const gray100 = "#f4f4f5";
+export const gray200 = "#e4e4e7";
+export const gray300 = "#d4d4d8";
+export const gray400 = "#a1a1aa";
+export const gray500 = "#71717a";
+export const gray600 = "#52525b";
+export const gray700 = "#3f3f46";
+export const gray800 = "#27272a";
+export const gray900 = "#18181b";
+export const gray950 = "#111111";
+
+export const gray = {
+  50: gray50,
+  100: gray100,
+  200: gray200,
+  300: gray300,
+  400: gray400,
+  500: gray500,
+  600: gray600,
+  700: gray700,
+  800: gray800,
+  900: gray900,
+  950: gray950,
+};
+
+export const red50 = "#fef2f2";
+export const red100 = "#fee2e2";
+export const red200 = "#fecaca";
+export const red300 = "#fca5a5";
+export const red400 = "#f87171";
+export const red500 = "#ef4444";
+export const red600 = "#dc2626";
+export const red700 = "#991919";
+export const red800 = "#511111";
+export const red900 = "#300c0c";
+export const red950 = "#1f0808";
+
+export const red = {
+  50: red50,
+  100: red100,
+  200: red200,
+  300: red300,
+  400: red400,
+  500: red500,
+  600: red600,
+  700: red700,
+  800: red800,
+  900: red900,
+  950: red950,
+};
+
+export const orange50 = "#fff7ed";
+export const orange100 = "#ffedd5";
+export const orange200 = "#fed7aa";
+export const orange300 = "#fdba74";
+export const orange400 = "#fb923c";
+export const orange500 = "#f97316";
+export const orange600 = "#ea580c";
+export const orange700 = "#92310a";
+export const orange800 = "#6c2710";
+export const orange900 = "#3b1106";
+export const orange950 = "#220a04";
+
+export const orange = {
+  50: orange50,
+  100: orange100,
+  200: orange200,
+  300: orange300,
+  400: orange400,
+  500: orange500,
+  600: orange600,
+  700: orange700,
+  800: orange800,
+  900: orange900,
+  950: orange950,
+};
+
+export const yellow50 = "#fefce8";
+export const yellow100 = "#fef9c3";
+export const yellow200 = "#fef08a";
+export const yellow300 = "#fde047";
+export const yellow400 = "#facc15";
+export const yellow500 = "#eab308";
+export const yellow600 = "#ca8a04";
+export const yellow700 = "#845209";
+export const yellow800 = "#713f12";
+export const yellow900 = "#422006";
+export const yellow950 = "#281304";
+
+export const yellow = {
+  50: yellow50,
+  100: yellow100,
+  200: yellow200,
+  300: yellow300,
+  400: yellow400,
+  500: yellow500,
+  600: yellow600,
+  700: yellow700,
+  800: yellow800,
+  900: yellow900,
+  950: yellow950,
+};
+
+export const green50 = "#f0fdf4";
+export const green100 = "#dcfce7";
+export const green200 = "#bbf7d0";
+export const green300 = "#86efac";
+export const green400 = "#4ade80";
+export const green500 = "#22c55e";
+export const green600 = "#16a34a";
+export const green700 = "#116932";
+export const green800 = "#124a28";
+export const green900 = "#042713";
+export const green950 = "#03190c";
+
+export const green = {
+  50: green50,
+  100: green100,
+  200: green200,
+  300: green300,
+  400: green400,
+  500: green500,
+  600: green600,
+  700: green700,
+  800: green800,
+  900: green900,
+  950: green950,
+};
+
+export const teal50 = "#f0fdfa";
+export const teal100 = "#ccfbf1";
+export const teal200 = "#99f6e4";
+export const teal300 = "#5eead4";
+export const teal400 = "#2dd4bf";
+export const teal500 = "#14b8a6";
+export const teal600 = "#0d9488";
+export const teal700 = "#0c5d56";
+export const teal800 = "#114240";
+export const teal900 = "#032726";
+export const teal950 = "#021716";
+
+export const teal = {
+  50: teal50,
+  100: teal100,
+  200: teal200,
+  300: teal300,
+  400: teal400,
+  500: teal500,
+  600: teal600,
+  700: teal700,
+  800: teal800,
+  900: teal900,
+  950: teal950,
+};
+
+export const blue50 = "#eff6ff";
+export const blue100 = "#dbeafe";
+export const blue200 = "#bfdbfe";
+export const blue300 = "#a3cfff";
+export const blue400 = "#60a5fa";
+export const blue500 = "#3b82f6";
+export const blue600 = "#2563eb";
+export const blue700 = "#173da6";
+export const blue800 = "#1a3478";
+export const blue900 = "#14204a";
+export const blue950 = "#0c142e";
+
+export const blue = {
+  50: blue50,
+  100: blue100,
+  200: blue200,
+  300: blue300,
+  400: blue400,
+  500: blue500,
+  600: blue600,
+  700: blue700,
+  800: blue800,
+  900: blue900,
+  950: blue950,
+};
+export const cyan50 = "#ecfeff";
+export const cyan100 = "#cffafe";
+export const cyan200 = "#a5f3fc";
+export const cyan300 = "#67e8f9";
+export const cyan400 = "#22d3ee";
+export const cyan500 = "#06b6d4";
+export const cyan600 = "#0891b2";
+export const cyan700 = "#0c5c72";
+export const cyan800 = "#134152";
+export const cyan900 = "#072a38";
+export const cyan950 = "#051b24";
+
+export const cyan = {
+  50: cyan50,
+  100: cyan100,
+  200: cyan200,
+  300: cyan300,
+  400: cyan400,
+  500: cyan500,
+  600: cyan600,
+  700: cyan700,
+  800: cyan800,
+  900: cyan900,
+  950: cyan950,
+};
+export const purple50 = "#faf5ff";
+export const purple100 = "#f3e8ff";
+export const purple200 = "#e9d5ff";
+export const purple300 = "#d8b4fe";
+export const purple400 = "#c084fc";
+export const purple500 = "#a855f7";
+export const purple600 = "#9333ea";
+export const purple700 = "#641ba3";
+export const purple800 = "#4a1772";
+export const purple900 = "#2f0553";
+export const purple950 = "#1a032e";
+
+export const purple = {
+  50: purple50,
+  100: purple100,
+  200: purple200,
+  300: purple300,
+  400: purple400,
+  500: purple500,
+  600: purple600,
+  700: purple700,
+  800: purple800,
+  900: purple900,
+  950: purple950,
+};
+
+export const pink50 = "#fdf2f8";
+export const pink100 = "#fce7f3";
+export const pink200 = "#fbcfe8";
+export const pink300 = "#f9a8d4";
+export const pink400 = "#f472b6";
+export const pink500 = "#ec4899";
+export const pink600 = "#db2777";
+export const pink700 = "#a41752";
+export const pink800 = "#6d0e34";
+export const pink900 = "#45061f";
+export const pink950 = "#2c0514";
+
+export const pink = {
+  50: pink50,
+  100: pink100,
+  200: pink200,
+  300: pink300,
+  400: pink400,
+  500: pink500,
+  600: pink600,
+  700: pink700,
+  800: pink800,
+  900: pink900,
+  950: pink950,
+};
+export const colors = {
+  black: {
+    100: black100,
+  },
+  white: {
+    0: white0,
+  },
+  whiteAlpha,
+  blackAlpha,
+  gray,
+  red,
+  orange,
+  yellow,
+  green,
+  teal,
+  blue,
+  cyan,
+  purple,
+  pink,
+};
