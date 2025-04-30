9b2170e6a8e4b63643e7837ae59c0687683a69a2
Fatemeh <uni.khosravi@gmail.com>
2025-04-30 11:38:00 +0330

    feat: add dark theme token groups

---
+++ packages/web-components/src/themes/dark.ts
@@ -0,0 +1,120 @@
+import { black, colors, rgba, white } from "colors";
+
+export const background = black;
+export const backgroundSubtle = colors.gray[950];
+export const backgroundMuted = colors.gray[900];
+export const backgroundEmphasized = colors.gray[800];
+export const backgroundInverted = white;
+export const backgroundPanel = colors.gray[950];
+export const backgroundError = colors.red[950];
+export const backgroundWarning = colors.orange[950];
+export const backgroundSuccess = colors.green[950];
+export const backgroundInfo = colors.blue[950];
+
+export const foreground = colors.gray[50];
+export const foregroundMuted = colors.gray[400];
+export const foregroundSubtle = colors.gray[500];
+export const foregroundInverted = black;
+export const foregroundError = colors.red[400];
+export const foregroundWarning = colors.orange[300];
+export const foregroundSuccess = colors.green[300];
+export const foregroundInfo = colors.blue[300];
+
+export const border = colors.gray[800];
+export const borderMuted = colors.gray[900];
+export const borderSubtle = colors.gray[950];
+export const borderEmphasized = colors.gray[700];
+export const borderInverted = colors.gray[200];
+export const borderError = colors.red[400];
+export const borderWarning = colors.orange[400];
+export const borderSuccess = colors.green[400];
+export const borderInfo = colors.blue[400];
+
+export const grayContrast = black;
+export const grayForeground = colors.gray[200];
+export const graySubtle = colors.gray[900];
+export const grayMuted = colors.gray[800];
+export const grayEmphasized = colors.gray[700];
+export const graySolid = white;
+export const grayFocusRing = colors.gray[400];
+
+export const redContrast = white;
+export const redForeground = colors.red[300];
+export const redSubtle = colors.red[900];
+export const redMuted = colors.red[800];
+export const redEmphasized = colors.red[700];
+export const redSolid = colors.red[600];
+export const redFocusRing = colors.red[400];
+
+export const orangeContrast = black;
+export const orangeForeground = colors.orange[300];
+export const orangeSubtle = colors.orange[900];
+export const orangeMuted = colors.orange[800];
+export const orangeEmphasized = colors.orange[700];
+export const orangeSolid = colors.orange[500];
+export const orangeFocusRing = colors.orange[400];
+
+export const greenContrast = white;
+export const greenForeground = colors.green[300];
+export const greenSubtle = colors.green[900];
+export const greenMuted = colors.green[800];
+export const greenEmphasized = colors.green[700];
+export const greenSolid = colors.green[600];
+export const greenFocusRing = colors.green[400];
+
+export const blueContrast = white;
+export const blueForeground = colors.blue[300];
+export const blueSubtle = colors.blue[900];
+export const blueMuted = colors.blue[800];
+export const blueEmphasized = colors.blue[700];
+export const blueSolid = colors.blue[600];
+export const blueFocusRing = colors.blue[400];
+
+export const yellowContrast = black;
+export const yellowForeground = colors.yellow[300];
+export const yellowSubtle = colors.yellow[900];
+export const yellowMuted = colors.yellow[800];
+export const yellowEmphasized = colors.yellow[700];
+export const yellowSolid = colors.yellow[300];
+export const yellowFocusRing = colors.yellow[400];
+
+export const tealContrast = white;
+export const tealForeground = colors.teal[300];
+export const tealSubtle = colors.teal[900];
+export const tealMuted = colors.teal[800];
+export const tealEmphasized = colors.teal[700];
+export const tealSolid = colors.teal[600];
+export const tealFocusRing = colors.teal[400];
+
+export const purpleContrast = white;
+export const purpleForeground = colors.purple[300];
+export const purpleSubtle = colors.purple[900];
+export const purpleMuted = colors.purple[800];
+export const purpleEmphasized = colors.purple[700];
+export const purpleSolid = colors.purple[600];
+export const purpleFocusRing = colors.purple[400];
+
+export const pinkContrast = white;
+export const pinkForeground = colors.pink[300];
+export const pinkSubtle = colors.pink[900];
+export const pinkMuted = colors.pink[800];
+export const pinkEmphasized = colors.pink[700];
+export const pinkSolid = colors.pink[600];
+export const pinkFocusRing = colors.pink[400];
+
+export const cyanContrast = white;
+export const cyanForeground = colors.cyan[300];
+export const cyanSubtle = colors.cyan[900];
+export const cyanMuted = colors.cyan[800];
+export const cyanEmphasized = colors.cyan[700];
+export const cyanSolid = colors.cyan[600];
+export const cyanFocusRing = colors.cyan[400];
+
+export const shadowXS = `0px 1px 1px ${rgba(black, 0.64)}, 0px 0px 1px inset ${rgba(colors.gray[300], 0.2)}`;
+export const shadowSM = `0px 2px 4px ${rgba(black, 0.64)}, 0px 0px 1px inset ${rgba(colors.gray[300], 0.3)}`;
+export const shadowMD = `0px 4px 8px ${rgba(black, 0.64)}, 0px 0px 1px inset ${rgba(colors.gray[300], 0.3)}`;
+export const shadowLG = `0px 8px 16px ${rgba(black, 0.64)}, 0px 0px 1px inset ${rgba(colors.gray[300], 0.3)}`;
+export const shadowXL = `0px 16px 24px ${rgba(black, 0.64)}, 0px 0px 1px inset ${rgba(colors.gray[300], 0.3)}`;
+export const shadow2XL = `0px 24px 40px ${rgba(black, 0.64)}, 0px 0px 1px inset ${rgba(colors.gray[300], 0.3)}`;
+export const shadowInner = `inset 0 2px 4px 0 ${black}`;
+export const shadowInset = `inset 0 0 0 1px ${rgba(colors.gray[300], 0.05)}`;
