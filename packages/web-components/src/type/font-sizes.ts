f0cf49a2e9cf1128163fca054c28da008dd0fdfc
Fatemeh <uni.khosravi@gmail.com>
2025-05-02 12:30:00 +0330

    feat: add font size tokens

---
+++ packages/web-components/src/type/font-sizes.ts
@@ -0,0 +1,18 @@
+import { rem } from "layout";
+
+export const fontSizes = {
+  "2xs": rem(10),
+  xs: rem(12),
+  sm: rem(14),
+  md: rem(16),
+  lg: rem(18),
+  xl: rem(20),
+  "2xl": rem(24),
+  "3xl": rem(30),
+  "4xl": rem(36),
+  "5xl": rem(48),
+  "6xl": rem(60),
+  "7xl": rem(72),
+  "8xl": rem(96),
+  "9xl": rem(128),
+};
