ced3ad437ef4b2294a70cb5278f299097fad8f67
Fatemeh <uni.khosravi@gmail.com>
2025-05-03 17:30:00 +0330

    feat: add font tokens

---
+++ packages/web-components/src/type/fonts.ts
@@ -0,0 +1,7 @@
+const fallback = `-apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"`;
+
+export const fontFamilies = {
+  heading: `Inter, ${fallback}`,
+  body: `Inter, ${fallback}`,
+  mono: `SFMono-Regular,Menlo,Monaco,Consolas,"Liberation Mono","Courier New",monospace`,
+};
