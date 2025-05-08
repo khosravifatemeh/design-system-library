753b696a03591b6efe2975789282c45ae7185e6d
Fatemeh <uni.khosravi@gmail.com>
2025-05-09 00:00:00 +0330

    refactor: made node definition type based and handle try catch

--- packages/web-components/src/scss-builder/ast/node-definition.ts
+++ packages/web-components/src/scss-builder/ast/node-definition.ts
@@ -1,89 +1,141 @@
-import p, { compose, newLine, print } from "../generate/printer.util";
-import { Node, StyleSheet } from "../types";
-import { convert, formats } from "../utils/token-format";
+import p from "../generate/printer";
+import t from "../generate/token";
+import {
+  Assignment,
+  AtReturn,
+  BlockStatement,
+  Identifier,
+  IfStatement,
+  LogicalExpression,
+  NewLine,
+  Node,
+  NodeDefinition,
+  SassBoolean,
+  SassFunction,
+  SassFunctionCall,
+  SassMap,
+  SassMapProperty,
+  SassModule,
+  SassNumber,
+  SassString,
+  SassValue,
+  StyleSheet,
+} from "../types";
+import { convertName } from "../utils/name-format";
 import { defineNode } from "./node-builder";
 import v from "./validators";
-const nodeTypes = {
-  IfStatement: "IfStatement",
-  SassMapProperty: "SassMapProperty",
-  SassMap: "SassMap",
-};
-const SassNumber = defineNode("SassNumber", {
+
+const SassNumber = defineNode<SassNumber>("SassNumber", {
   fields: {
     value: {
       validate: v.validateValueType("number"),
     },
   },
-  generate: (node) => {
-    return node.value;
+  generate: (node: SassNumber) => {
+    try {
+      return node.value;
+    } catch (error) {
+      throw new Error(`Unexpected error in ${node.type} - ${error.message}`);
+    }
   },
 });
-const SassModule = defineNode("SassModule", {
+const SassModule = defineNode<SassModule>("SassModule", {
   fields: { path: { validate: v.validateValueType("string") } },
-  generate: (node) => p.compose("@use", p.space(), `'${node.path}'`, ";"),
+  generate: (node: SassModule) => {
+    try {
+      return p.compose(t.module, t.space, `'${node.path}'`, t.semicolon);
+    } catch (error) {
+      throw new Error(`Unexpected error in ${node.type} - ${error.message}`);
+    }
+  },
 });
-const SassValue = defineNode("SassValue", {
+const SassValue = defineNode<SassValue>("SassValue", {
   fields: {
     value: {
       validate: v.validateAny,
     },
   },
-  generate: (node) => {
-    return node.value;
+  generate: (node: SassValue) => {
+    try {
+      return node.value;
+    } catch (error) {
+      throw new Error(`Unexpected error in ${node.type} - ${error.message}`);
+    }
   },
 });
-const SassString = defineNode("SassString", {
+const SassString = defineNode<SassString>("SassString", {
   fields: {
     value: {
       validate: v.validateValueType("string"),
     },
   },
-  generate: (node) => {
-    return `'${node.value}'`;
+  generate: (node: SassString) => {
+    try {
+      return `'${node.value}'`;
+    } catch (error) {
+      throw new Error(`Unexpected error in ${node.type} - ${error.message}`);
+    }
   },
 });
-const AtReturn = defineNode("AtReturn", {
+const AtReturn = defineNode<AtReturn>("AtReturn", {
   fields: { argument: { validate: v.validateAny } },
-  generate: (node, parent) => {
-    const needNewLine = parent?.body?.indexOf(node) !== 0 ? p.newLine(0) : "";
-    return compose(
-      needNewLine,
-      newLine(0),
-      "@return",
-      p.space(),
-      p.print(node.argument)
-    );
+  generate: (node: AtReturn, parent: Node) => {
+    try {
+      const needNewLine =
+        "body" in parent &&
+        Array.isArray(parent.body) &&
+        parent?.body?.indexOf(node) !== -1
+          ? p.newLine(0)
+          : t.empty;
+      return p.compose(
+        needNewLine,
+        p.newLine(0),
+        t.return,
+        t.space,
+        p.print(node.argument)
+      );
+    } catch (error) {
+      throw new Error(`Unexpected error in ${node.type} - ${error.message}`);
+    }
   },
 });
-const BlockStatement = defineNode("BlockStatement", {
+const BlockStatement = defineNode<BlockStatement>("BlockStatement", {
   fields: {
     body: {
       validate: v.validateArrayOf(v.validateAny),
     },
   },
-  generate: (node) => {
-    if (!node.body?.length) return "{}";
-    const body = node.body.map((child, index) =>
-      compose(
-        p.newLine(2),
-        p.print(child, node),
-        index < node.body.length - 1 ? p.newLine(2) : ""
-      )
-    );
-    return compose(p.space(), "{", ...body, newLine(1), "}");
+  generate: (node: BlockStatement) => {
+    try {
+      if (!node.body?.length) return t.openBrace + t.closeBrace;
+      const body = node.body.map((child, index) =>
+        p.compose(
+          p.newLine(2),
+          p.print(child, node),
+          index < node.body.length - 1 ? p.newLine(2) : t.empty
+        )
+      );
+      return p.compose(
+        t.space,
+        t.openBrace,
+        ...body,
+        p.newLine(1),
+        t.closeBrace
+      );
+    } catch (error) {
+      throw new Error(`Unexpected error in ${node.type} - ${error.message}`);
+    }
   },
 });
-const LogicalExpression = defineNode("LogicalExpression", {
+const LogicalExpression = defineNode<LogicalExpression>("LogicalExpression", {
   fields: {
     left: { validate: v.validateAny },
     operator: { validate: v.validateValueType("string") },
     right: { validate: v.validateAny },
   },
-  generate: (node) => {
+  generate: (node: LogicalExpression) => {
     try {
-      console.log("Hi Logical");
-
-      return compose(
+      return p.compose(
         p.print(node.left, node),
         p.space(),
         node.operator,
@@ -91,58 +143,49 @@ const LogicalExpression = defineNode("LogicalExpression", {
         p.print(node.right, node)
       );
     } catch (error) {
-      throw new Error("Logical ***************");
+      throw new Error(`Unexpected error in ${node.type} - ${error.message}`);
     }
   },
 });
 
-const IfStatement = defineNode("IfStatement", {
+const IfStatement = defineNode<IfStatement>("IfStatement", {
   fields: {
     test: { validate: v.validateAny },
     consequent: {
       required: false,
-      validate: v.validateType(BlockStatement),
+      validate: v.validateType("BlockStatement"),
     },
     alternate: {
       required: false,
       validate: v.validateOneOf([
-        // @ts-ignore - ignoring type checking for alternate
         v.validateType("IfStatement"),
-        v.validateType(BlockStatement),
+        v.validateType("BlockStatement"),
       ]),
     },
   },
-  generate: (node, parent) => {
+  generate: (node: IfStatement, parent: Node) => {
     try {
-      console.log(
-        "/Hiiiiiiiiiiiiii********",
-        node.test,
-        node.consequent,
-        parent
-      );
-
-      const innerSpace =
+      const ifKeyword =
         parent && parent.type === IfStatement.type
-          ? p.compose(p.space(), "if")
-          : "@if";
+          ? p.compose(p.space(), t.if)
+          : t.atIf;
       const alternate = node.alternate
-        ? p.compose("@else", p.print(node.alternate, node))
-        : "";
-      console.log("IfStatement", node.test, node.consequent);
-      return compose(
-        innerSpace,
-        p.space(),
+        ? p.compose(t.atElse, p.print(node.alternate, node))
+        : t.empty;
+      return p.compose(
+        ifKeyword,
+        t.space,
         p.print(node.test, node),
         p.print(node.consequent, node),
         alternate
       );
     } catch (error) {
-      throw new Error("IF Error ******************");
+      throw new Error(`Unexpected error in ${node.type} - ${error.message}`);
     }
   },
 });
 
-const StyleSheet = defineNode("StyleSheet", {
+const StyleSheet = defineNode<StyleSheet>("StyleSheet", {
   fields: {
     children: {
       validate: () =>
@@ -156,23 +199,36 @@ const StyleSheet = defineNode("StyleSheet", {
         ),
     },
   },
-  generate: (node: StyleSheet, parent: Node) => {
-    return node.children.map((n: Node, index: number) =>
-      compose(print(n), index < node.children.length - 1 ? newLine(0) : "")
-    );
+  generate: (node: StyleSheet, _) => {
+    try {
+      return node.children
+        .map((n: Node, index: number) =>
+          p.compose(
+            p.print(n),
+            index < node.children.length - 1 ? p.newLine(0) : t.empty
+          )
+        )
+        .join("");
+    } catch (error) {
+      throw new Error(`Unexpected error in ${node.type} - ${error.message}`);
+    }
   },
 });
-const Identifier = defineNode("Identifier", {
+const Identifier = defineNode<Identifier>("Identifier", {
   fields: {
     name: { validate: v.validateValueType("string") },
     kebabCase: { validate: v.validateValueType("boolean") },
   },
-  generate: (node, parent) => {
-    const name = node.kebabCase ? convert(node.name, formats.scss) : node.name;
-    return p.compose(parent ? "$" : "", name);
+  generate: (node: Identifier, parent: Node) => {
+    try {
+      const name = node.kebabCase ? convertName(node.name) : node.name;
+      return p.compose(parent ? t.variablePrefix : t.empty, name);
+    } catch (error) {
+      throw new Error(`Unexpected error in ${node.type} - ${error.message}`);
+    }
   },
 });
-const SassFunctionCall = defineNode("SassFunctionCall", {
+const SassFunctionCall = defineNode<SassFunctionCall>("SassFunctionCall", {
   fields: {
     id: {
       validate: v.validateType(Identifier),
@@ -188,16 +244,33 @@ const SassFunctionCall = defineNode("SassFunctionCall", {
       ),
     },
   },
-  generate: (node, parent) => {
-    const properties = node.params.map((prop, index) => {
-      const dollarSign = prop.type === Identifier.type ? "$" : "";
-      const semi = index < node.params.length ? "," : "";
-      return p.compose(dollarSign, p.print(prop, node), semi, p.space());
-    });
-    return p.compose(p.space(), p.print(node.id), "(", ...properties, ")");
+  generate: (node: SassFunction, _) => {
+    try {
+      const properties = node.params.map((prop, index) => {
+        const needVariablePrefix =
+          prop.type === Identifier.type ? t.variablePrefix : t.empty;
+        const comma = index < node.params.length ? t.semicolon : t.empty;
+
+        return p.compose(
+          needVariablePrefix,
+          p.print(prop, node),
+          comma,
+          p.space()
+        );
+      });
+      return p.compose(
+        t.space,
+        p.print(node.id),
+        t.openParen,
+        ...properties,
+        t.closeParen
+      );
+    } catch (error) {
+      throw new Error(`Unexpected error in ${node.type} - ${error.message}`);
+    }
   },
 });
-const SassMapProperty = defineNode("SassMapProperty", {
+const SassMapProperty = defineNode<SassMapProperty>("SassMapProperty", {
   fields: {
     key: {
       validate: v.validateType(Identifier),
@@ -214,32 +287,41 @@ const SassMapProperty = defineNode("SassMapProperty", {
       validate: v.validateValueType("boolean"),
     },
   },
-  generate: (node, parent) => {
-    const key = node.quoted
-      ? compose("'", p.print(node.key), "'")
-      : p.print(node.key);
-    return p.compose(key, ":", p.space(), p.print(node.value, node));
+  generate: (node: SassMapProperty, _) => {
+    try {
+      const key = node.quoted
+        ? p.compose(t.singleQuote, p.print(node.key), t.singleQuote)
+        : p.print(node.key);
+      return p.compose(key, t.colon, p.space(), p.print(node.value, node));
+    } catch (error) {
+      throw new Error(`Unexpected error in ${node.type} - ${error.message}`);
+    }
   },
 });
 
-const SassMap = defineNode("SassMap", {
+const SassMap = defineNode<SassMap>("SassMap", {
   fields: {
     properties: {
       validate: v.validateArrayOf(v.validateType(SassMapProperty)),
     },
   },
-  generate: (node, parent) => {
-    if (!node.properties.length) {
-      return "()";
+  generate: (node: SassMap, _) => {
+    try {
+      if (!node.properties.length) {
+        return t.openBrace + t.closeBrace;
+      }
+      const properties = node.properties.map((prop, index) => {
+        const isLast =
+          index < node.properties.length - 1 ? t.separator : t.empty;
+        return p.compose(p.newLine(2), p.print(prop), isLast);
+      });
+      return p.compose(t.openBrace, ...properties, p.newLine(0), t.closeBrace);
+    } catch (error) {
+      throw new Error(`Unexpected error in ${node.type} - ${error.message}`);
     }
-    const properties = node.properties.map((prop, index) => {
-      const isLast = index < node.properties.length - 1 ? "," : "";
-      return compose(p.newLine(2), p.print(prop), isLast);
-    });
-    return compose("(", ...properties, p.newLine(0), ")");
   },
 });
-const Assignment = defineNode("Assignment", {
+const Assignment = defineNode<Assignment>("Assignment", {
   fields: {
     id: {
       validate: v.validateType(Identifier),
@@ -257,38 +339,44 @@ const Assignment = defineNode("Assignment", {
       validate: v.validateValueType("boolean"),
     },
   },
-  generate: (node, parent) => {
-    const defaultSection = p.compose(p.space(), "!default");
-    return p.compose(
-      p.print(node.id, node),
-      ":",
-      p.space(),
-      p.print(node.init, node),
-      defaultSection,
-      ";"
-    );
+  generate: (node: Assignment, _) => {
+    try {
+      const defaultSection = p.compose(p.space(), t.default);
+      return p.compose(
+        p.print(node.id, node),
+        t.colon,
+        t.space,
+        p.print(node.init, node),
+        defaultSection,
+        t.semicolon
+      );
+    } catch (error) {
+      throw new Error(`Unexpected error in ${node.type} - ${error.message}`);
+    }
   },
 });
 
-const NewLine = defineNode("NewLine", {
+const NewLine = defineNode<NewLine>("NewLine", {
   generate: () => {
     return p.newLine(0);
   },
 });
-const SassBoolean = defineNode("SassBoolean", {
+const SassBoolean = defineNode<SassBoolean>("SassBoolean", {
   fields: {
     value: {
       validate: v.validateValueType("boolean"),
     },
   },
-  generate: (node) => {
-    console.log("8888888888888888888888", node);
-
-    return node.value ? "true" : "false";
+  generate: (node: SassBoolean) => {
+    try {
+      return node.value ? "true" : "false";
+    } catch (error) {
+      throw new Error(`Unexpected error in ${node.type} - ${error.message}`);
+    }
   },
 });
 
-const SassFunction = defineNode("SassFunction", {
+const SassFunction = defineNode<SassFunction>("SassFunction", {
   fields: {
     id: {
       validate: v.validateType(Identifier),
@@ -303,23 +391,39 @@ const SassFunction = defineNode("SassFunction", {
       validate: v.validateType(BlockStatement),
     },
   },
-  generate: (node, parent) => {
-    const first = compose(
-      "@function",
-      p.space(),
-      p.print(node.id, parent),
-      "("
-    );
+  generate: (node: SassFunction, parent: Node) => {
+    try {
+      const firstSection = p.compose(
+        t.function,
+        t.space,
+        p.print(node.id, parent),
+        t.openBrace
+      );
 
-    const middle = node.params.map((child, index) => {
-      const a = index < node.params.length ? compose(",", p.space()) : "";
-      return p.compose(p.print(child, node), a);
-    });
+      const middleSection = node.params
+        .map((child, index) => {
+          const needSeparator =
+            index < node.params.length
+              ? p.compose(t.separator, p.space())
+              : t.empty;
+          return p.compose(p.print(child, node), needSeparator);
+        })
+        .join();
 
-    return compose(first, middle, ")", p.space(), p.print(node.body, parent));
+      return p.compose(
+        firstSection,
+        middleSection,
+        t.closeBrace,
+        t.space,
+        p.print(node.body, parent)
+      );
+    } catch (error) {
+      throw new Error(`Unexpected error in ${node.type} - ${error.message}`);
+    }
   },
 });
-export default {
+
+export const definitions = {
   SassModule,
   StyleSheet,
   SassValue,
