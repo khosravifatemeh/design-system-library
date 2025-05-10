37029ad8906a5c6bf30511c3c0f358864c118afd
Fatemeh <uni.khosravi@gmail.com>
2025-05-10 20:43:36 +0330

    refactor layout build scss

--- packages/web-components/src/layout/tasks/build.ts
+++ packages/web-components/src/layout/tasks/build.ts
@@ -1,67 +1,90 @@
 import { mkdirSync } from "fs";
 import { writeFile } from "fs/promises";
 import path from "path";
-import { generate, primitive, types as t } from "scss-builder";
+import { generate, util as u, types as t } from "scss-builder";
 import { breakpoints, sizes, spacing, zIndices } from "../index";
 
-function buildTokens(group, list) {
-  const listVariables = [];
-  const Variablesinfo = [];
+interface BuildFile {
+  filepath: string;
+  builder: any;
+}
 
-  Object.entries(list).forEach(([key, value]) => {
-    const id = t.Identifier(key, true);
-    const assignment = t.Assignment({
-      id,
-      init: primitive(value),
+function getMap(prefix, list) {
+  if (typeof list !== "object") {
+    return u.primitive(`$${prefix}`);
+  }
+  return t.SassMap({
+    properties: Object.entries(list).map(([key, value]) => {
+      const name = u.joinKeys(prefix, key);
+      return t.SassMapProperty({
+        key: t.Identifier(key, true),
+        value: getMap(name, value),
+      });
+    }),
+  });
+}
+
+function getVariables(prefix, values) {
+  return Object.entries(values).flatMap(([key, value]) => {
+    if (typeof value === "object") {
+      const name = u.joinKeys(prefix, key);
+      return getVariables(name, value);
+    }
+    return t.Assignment({
+      id: t.Identifier(u.joinKeys(prefix, key), true),
+      init: u.primitive(value),
       default: true,
     });
-    listVariables.push(t.NewLine(), assignment);
-    Variablesinfo.push([id, key]);
   });
-
-  const listMap = t.Assignment({
-    id: t.Identifier(group),
-    init: t.SassMap({
-      properties: Variablesinfo.map(([id, key]) => {
-        return t.SassMapProperty({
-          key: id,
-          value: primitive(`$${key}`),
-        });
-      }),
-    }),
-    default: true,
-  });
-  return t.StyleSheet([...listVariables, t.NewLine(), listMap]);
-}
-function generateTokens(tokens) {
-  const { code } = generate(tokens);
-  return code;
-}
-function generateZindex() {
-  return generateTokens(buildTokens("z-index", zIndices));
-}
-function generateSpacing() {
-  return generateTokens(buildTokens("spacing", spacing));
-}
-function generateBreakpoints() {
-  return generateTokens(buildTokens("breakpoint", breakpoints));
-}
-function generateSizes() {
-  return generateTokens(buildTokens("size", sizes));
 }
 
+function buildTokens(prefix, list) {
+  const layoutVariables = getVariables(null, list);
+  const layoutMap = t.Assignment({
+    id: t.Identifier(prefix),
+    init: getMap(null, list),
+  });
+
+  return t.StyleSheet([
+    t.NewLine(),
+    ...layoutVariables,
+    t.NewLine(),
+    layoutMap,
+  ]);
+}
+
+export function getGeneratedPath(module: string) {
+  const SRC_DIR = process.env.SRC_DIR || path.resolve(__dirname, "../");
+  return path.join(SRC_DIR, module, "scss", "generated");
+}
 async function build() {
-  const generatedDir = path.resolve(__dirname, "../../scss/generated");
+  const generatedDir = getGeneratedPath("layout");
+
+  const files: BuildFile[] = [
+    {
+      filepath: path.join(generatedDir, "_spacing.scss"),
+      builder: buildTokens("spacing", spacing),
+    },
+    {
+      filepath: path.join(generatedDir, "_zindex.scss"),
+      builder: buildTokens("z-index", zIndices),
+    },
+    {
+      filepath: path.join(generatedDir, "_breakpoint.scss"),
+      builder: buildTokens("breakpoint", breakpoints),
+    },
+    {
+      filepath: path.join(generatedDir, "_size.scss"),
+      builder: buildTokens("size", sizes),
+    },
+  ];
   mkdirSync(generatedDir, { recursive: true });
-  const filePath1 = path.join(generatedDir, "_spacing.scss");
-  const filePath2 = path.join(generatedDir, "_zindex.scss");
-  const filePath3 = path.join(generatedDir, "_breakpoint.scss");
-  const filePath4 = path.join(generatedDir, "_size.scss");
   try {
-    await writeFile(filePath1, generateSpacing());
-    await writeFile(filePath2, generateZindex());
-    await writeFile(filePath3, generateBreakpoints());
-    await writeFile(filePath4, generateSizes());
+    for (const { filepath, builder } of files) {
+      const { code } = generate(builder as any);
+      await writeFile(filepath, code);
+    }
+
     console.log("Layout SCSS has been generated successfully");
   } catch (error) {
     console.error("error writing Layout SCSS files:", error);
