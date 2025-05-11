5042670bf39fd5691cccd0d02e3de7e097a4c8c5
Fatemeh <uni.khosravi@gmail.com>
2025-05-11 12:19:12 +0330

    removed themes

--- packages/web-components/src/themes/index.ts
+++
@@ -1,6 +0,0 @@
-import * as light from "./light";
-import * as dark from "./dark";
-export const themes = {
-  light,
-  dark,
-};
--- packages/web-components/src/themes/tokens/borders.ts
+++
@@ -1,7 +0,0 @@
-import { TokenGroup } from "./base/token-group";
-
-export const borderTokens = TokenGroup.create({
-  name: "Border",
-  properties: [],
-  tokens: ["border-xs", "border-sm", "border-md", "border-lg", "border-xl"],
-});
